45ad0e9858fe0adf1abdead0533a9df916b4b324
Chaicoo <gabriellimma27@gmail.com>
2025-01-01 19:58:18 -0300

    refactor: remove welcome route and simplify SpotifyConverter route

--- routes/web.php
+++ routes/web.php
@@ -7,15 +7,6 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
-});
-
-Route::get('/test-convert', function () {
     return Inertia::render('SpotifyConverter');
 });
 
